2f878913a58ce24b93cf8fb5f161a6a20f8231a1
Jin <jbjxu@thoughtworks.com>
2020-02-15 12:00:12 +0800

    feat: change the return data type of fibonacci calculate to long

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,7 +2,7 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
 
-    public int calculate(int position) {
+    public long calculate(int position) {
         if (position == 1 || position == 2) {
             return 1;
         }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -18,28 +18,35 @@ class FibonacciTest {
     @DisplayName("当求斐波那契数列的第 1 位时, 返回 1")
     @Test
     void should_return_1_when_calculate_given_position_is_1() {
-        int result = fibonacci.calculate(1);
+        long result = fibonacci.calculate(1);
         Assertions.assertEquals(1, result);
     }
 
     @DisplayName("当求斐波那契数列的第 2 位时, 返回 1")
     @Test
     void should_return_1_when_calculate_given_position_is_2() {
-        int result = fibonacci.calculate(2);
+        long result = fibonacci.calculate(2);
         Assertions.assertEquals(1, result);
     }
 
     @DisplayName("当求斐波那契数列的第 3 位时, 返回 2")
     @Test
     void should_return_2_when_calculate_given_position_is_3() {
-        int result = fibonacci.calculate(3);
+        long result = fibonacci.calculate(3);
         Assertions.assertEquals(2, result);
     }
 
     @DisplayName("当求斐波那契数列的第 4 位时, 返回 3")
     @Test
     void should_return_3_when_calculate_given_position_is_4() {
-        int result = fibonacci.calculate(4);
+        long result = fibonacci.calculate(4);
         Assertions.assertEquals(3, result);
+    }
+
+    @DisplayName("当求斐波那契数列的第 50 位时, 返回 12586269025L")
+    @Test
+    void should_return_12586269025L_when_calculate_given_position_is_50() {
+        long result = fibonacci.calculate(50);
+        Assertions.assertEquals(12586269025L, result);
     }
 }
