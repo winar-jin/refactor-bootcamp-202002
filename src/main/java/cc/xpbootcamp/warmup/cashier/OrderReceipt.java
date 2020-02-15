8a81ec9a9aba71e06a306c64f5a8508c7dcb7619
Jin <jbjxu@thoughtworks.com>
2020-02-15 13:26:33 +0800

    refactor: correct the  comment more meaningful

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -20,11 +20,9 @@ public class OrderReceipt {
         // print headers
         output.append("======Printing Orders======\n");
 
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
+        // print customer name, customer address
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
 
         // prints lineItems
         double totalSalesTax = 0d;
